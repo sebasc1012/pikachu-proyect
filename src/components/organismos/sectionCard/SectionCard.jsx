c1f483de92d14744860399218f76222b2ad0c0c1
sebastian <juan.castano@blankfactor.com>
2024-07-30 08:49:32 -0500

    add something

--- src/components/organismos/sectionCard/SectionCard.jsx
+++ src/components/organismos/sectionCard/SectionCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useFetch } from '../../../../hooks/useFetch'
 import style from '../sectionCard/SectionCard.module.scss'
+import { useFetch } from '../../../hooks/useFetch'
 
 export const SectionCard = () => {
 
